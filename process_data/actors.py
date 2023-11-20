15e3f7e474b3dfbea648d43d8b148f45134a4cf5
ahfetea <ahfetea@yahoo.com>
2023-11-19 19:24:29 -0500

    updates to visualizations

--- process_data/actors.py
+++ process_data/actors.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 
 
@@ -7,15 +8,18 @@ def get_person_popularity(name):
     # API endpoint for searching a person
     url = "https://api.themoviedb.org/3/search/person"
 
+    # Fetch the API key from environment variables
+    api_key = os.getenv('TMDB_API_KEY')
+
     # Your API key (Bearer token)
     headers = {
         "accept": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTg5ODJiMDdhNGRkZWE4ZjBmOTI4NjRmMzU5MmVhOCIsInN1YiI6IjY1NWE3YjEwMjIyMmY2MDEzOTJhYzRmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1YgoNj90nNur3bG7POfzZpcqEhDP92NRyCWk7k9QLa4"
+        "Authorization": f"Bearer {api_key}"
     }
 
     # Parameters for the search
     params = {
-        'api_key': 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTg5ODJiMDdhNGRkZWE4ZjBmOTI4NjRmMzU5MmVhOCIsInN1YiI6IjY1NWE3YjEwMjIyMmY2MDEzOTJhYzRmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1YgoNj90nNur3bG7POfzZpcqEhDP92NRyCWk7k9QLa4',  # Replace with your actual API Key
+        'api_key': api_key,  # Use the API Key from the environment variable
         'query': name
     }
 
